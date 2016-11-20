9990b0bd1fff2e45eb1fd20f4aa0a77793a1588c
Alexander Dayneko <dayneko.alexander@gmail.com>
2016-11-21 01:06:43 +0300

    Changed article markup, styles and added attribution link

--- Day1/js/main.js
+++ Day1/js/main.js
@@ -35,23 +35,48 @@ articlesFetched.then(
 
 		for(let article of articles){
 
-			let title = document.createElement('h2');
-			title.className += ('title');
-			title.innerText = article.title;
+			let articleElement = document.createElement('article');
+
+			let urlElement = document.createElement('a');
+			urlElement.href = article.url;
+
+			let container = document.createElement('div');
+			container.className += ('article-container');
 
 			let image = document.createElement('img');
 			image.src = article.urlToImage;
 
+			let textContainer = document.createElement('div');
+			textContainer.className += ('text-container');
+
+			let header = document.createElement('header');
+
+			let title = document.createElement('h2');
+			title.className += ('title');
+			title.innerText = article.title;
+
 			let description = document.createElement('p');
 			description.innerText = article.description;
 
-			let header = document.createElement('header');
-			header.append(image);
-			header.append(title);
+			let time = document.createElement('time');
+			time.innerText = article.publishedAt;
 
-			let articleElement = document.createElement('article');
-			articleElement.append(header);
-			articleElement.append(description);
+			let author = document.createElement('span');
+			author.innerText = article.author;
+
+			header.append(title);
+			header.append(author);
+			header.append(time);
+
+			textContainer.append(header);
+			textContainer.append(description);
+
+			container.append(image);
+			container.append(textContainer);
+
+			urlElement.append(container);
+
+			articleElement.appendChild(urlElement);
 			articlesElement.appendChild(articleElement);
 		}
 	},
